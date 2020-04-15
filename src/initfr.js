66bc1a19bb7768f7a18281f1021f265c57ec7973
denyncrawford <kiquerangelb@gmail.com>
2020-04-15 11:25:06 -0300

    First commit

---
+++ src/initfr.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import VueFlyRoutes from 'vue-flyroutes'
+import {pageTransition, contentAnimation} from './transitions'
+
+Vue.use(VueFlyRoutes, {
+  transitions: [
+    {
+      leave(data, done) {
+        pageTransition();
+        setTimeout(() => {
+          done()
+        },1500)
+      },
+      enter(data) {
+        contentAnimation();
+      },
+    }
+  ]
+})
